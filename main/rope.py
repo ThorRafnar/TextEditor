a156daf1b902d83e8cdfbf861605da6fd63d3a24
ThorRafnar <thorsteinningis@gmail.com>
2024-01-17 09:47:52 +0200

    Implemented balance, split, and insert operations. Added several unit test cases for over 83% coverage. Added performance test vs pythons base str data type. Str is better with smaller strings through fewer inserts, but rope outperforms str when working with very large strings and many inserts. This is the expected result.

---
+++ main/rope.py
@@ -0,0 +1,200 @@
+from __future__ import annotations
+from typing import Union, List, Optional
+
+class Rope:
+    def __init__(self, data: Union[str, List[str]] = '') -> None:
+        # STRING BASED CONSTRUCTOR
+        MAX_LEAF_SIZE = 32
+        if isinstance(data, str):
+            self.left = None
+            self.right = None
+            self.data = data
+            self.length = len(data)
+        # LIST BASED CONSTRUCTOR
+        elif isinstance(data, list):
+            # Assert every item is a string
+            for item in data:
+                if not isinstance(item, str):
+                    raise TypeError(f'Unsupported data type: {type(item)} found in list. Supported data types: list[str], str')
+            # Construct Rope based on length
+            if len(data) == 0:
+                self.__init__()
+            elif len(data) == 1:
+                # String based constructor handles splitting up if too long
+                self.__init__(data[0])
+            else:
+                # Round-up division
+                mid = len(data) // 2 + (len(data) % 2 > 0)
+                self.left = Rope(data[:mid])
+                self.right = Rope(data[mid:])
+                self.data = ''
+                self.length = self.left.length + self.right.length
+        else:
+            raise TypeError(f'Unsupported data type: {type(data)}. Supported data types: list[str], str')
+        self.current = self
+
+    def __eq__(self, other: Rope) -> bool:
+        return str(self) == str(other)
+
+    def __add__(self, other: Union[Rope, str, List[str]]) -> Rope:
+        if isinstance(other, Rope):
+            r = Rope()
+            r.left = self
+            r.right = other
+            r.length = self.length + other.length
+            r.current = self
+            r.balance()
+            return r
+        else:
+            try:
+                return self.__add__(Rope(other))
+            except Exception as e:
+                e(f'Unable to construct rope using {type(other)}, supported types: str, list[str]')
+                raise
+
+    def __len__(self) -> int:
+        if self.left is not None and self.right is not None:
+            return len(self.left) + len(self.right)
+        else:
+            return(len(self.data))
+
+    def __getitem__(self, index) -> Rope:
+        if isinstance(index, int):
+            if self.left and self.right:
+                if index < -self.right.length:
+                    subindex = index + self.right.length
+                elif index >= self.left.length:
+                    subindex = index - self.left.length
+                else:
+                    subindex = index
+
+                if index < -self.right.length or 0 <= index < self.left.length:
+                    return self.left[subindex]
+                else:
+                    return self.right[subindex]
+            else:
+                try:
+                    return Rope(self.data[index])
+                except IndexError:
+                    raise IndexError
+        else:
+            raise TypeError("Unsupported indexing")
+
+    def __repr__(self, level: int=0, prefix: str="Root: ") -> str:
+        # Representation for the current node
+        result = "  " * level + prefix + repr(self.data) + "\n"
+        
+        # If the current node has children, recursively represent them
+        if self.left is not None:
+            result += self.left.__repr__(level + 1, "Left: ")
+        if self.right is not None:
+            result += self.right.__repr__(level + 1, "Right: ")
+        
+        return result
+
+    def __str__(self) -> str:
+        if self.left is not None and self.right is not None:
+            return self.left.__str__() + self.right.__str__()
+        else:
+            return self.data
+
+    def __iter__(self) -> Rope:
+        return self
+
+    def __next__(self) -> str:
+        if self.current:
+            if self.left is not None and self.right is not None:
+                try:
+                    return next(self.left)
+                except StopIteration:
+                    self.current = self.right
+                return next(self.right)
+            else:
+                self.current = None
+                return self.data
+        else:
+            raise StopIteration
+
+    def next(self) -> str:
+        return self.__next__()
+    
+    def split(self, index: int) -> (Rope, Rope):
+        if index < 0:
+            index += len(self)
+        if index < 0 or index > len(self):
+            raise IndexError("Index out of bounds")
+        
+        # Base case: If the current node is a leaf node
+        if self.left is None and self.right is None:
+            left_part = Rope(self.data[:index])
+            right_part = Rope(self.data[index:])
+            return left_part, right_part
+
+        # Recursive case: Split occurs in the left or right child
+        if self.left and index <= len(self.left):
+            left_part, extra = self.left.split(index)
+            right_part = extra + self.right if self.right else extra
+        else:
+            extra, right_part = self.right.split(index - len(self.left))
+            left_part = self.left + extra
+
+        return left_part, right_part
+
+    def balance_factor(self) -> int:
+        left_height = self.left._height() if self.left else 0
+        right_height = self.right._height() if self.right else 0
+        return left_height - right_height
+
+    def balance(self):
+        bf = self.balance_factor()
+        if bf > 1:  # Left heavy
+            if self.left and self.left.balance_factor() < 0:
+                # Left-Right Case
+                self.left = self.left.rotate_left()
+            # Left-Left Case
+            return self.rotate_right()
+        elif bf < -1:  # Right heavy
+            if self.right and self.right.balance_factor() > 0:
+                # Right-Left Case
+                self.right = self.right.rotate_right()
+            # Right-Right Case
+            return self.rotate_left()
+        # Update the length and return the node itself if it's already balanced
+        self.update_length()
+        return self
+
+    def rotate_left(self):
+        new_root = self.right
+        self.right = new_root.left
+        new_root.left = self
+        self.update_length()
+        new_root.update_length()
+        return new_root
+
+    def rotate_right(self):
+        new_root = self.left
+        self.left = new_root.right
+        new_root.right = self
+        self.update_length()
+        new_root.update_length()
+        return new_root
+
+    def update_length(self) -> None:
+        self.length = len(self.data) + (self.left.length if self.left else 0) + (self.right.length if self.right else 0)
+
+    def _height(self):
+        if self.left is None and self.right is None:
+            return 0
+        left_height = self.left._height() + 1 if self.left else 0
+        right_height = self.right._height() + 1 if self.right else 0
+        return max(left_height, right_height)
+
+    def insert(self, index, s) -> None:
+        left, right = self.split(index)
+        insertion = Rope(s)
+        combined = left + insertion + right
+        self.left = combined.left
+        self.right = combined.right
+        self.data = combined.data
+        self.length = combined.length
+        self.balance()
---
+++ test/rope_test.py
@@ -0,0 +1,162 @@
+import unittest
+import timeit
+import sys
+
+sys.path.append('../')
+from main.rope import Rope
+
+class TestRopeMethods(unittest.TestCase):
+
+    def test_init_throws_type_error(self):
+        self.assertRaises(TypeError, Rope, {})
+        self.assertRaises(TypeError, Rope, ("this is", "not a string", "but a tuple"))
+        self.assertRaises(TypeError, Rope, "Two strings", "Are not good")
+        self.assertRaises(TypeError, Rope, ["A list with strings", 1, 2, "and ints","are not good"])
+    
+    def test_init_base(self):
+        Rope()
+
+    def test_init_accepts_string(self):
+        Rope("String")
+
+    def test_init_accepts_list_of_strings(self):
+        Rope(["List ", "of ", "strings"])
+
+    def test_init_accepts_empty_list(self):
+        Rope([])
+
+    def test_str_when_constructed_from_string(self):
+        my_string = "Hello, world"
+        self.assertEqual(str(Rope(my_string)), my_string)
+
+    def test_str_for_long_string(self):
+        my_string = "Hello, world! What a good day to be alive when we can supporty such long strings!!!"
+        self.assertEqual(str(Rope(my_string)), my_string)
+
+    def test_equal_ropes_with_different_constructors(self):
+        one = Rope("Hello, world")
+        two = Rope(["Hel", "lo,", " wo", "rld"])
+        self.assertEqual(one, two)
+
+    def test_unequal_ropes(self):
+        one = Rope("Hello, world")
+        two = Rope(["Good morning Viet Nam"])
+        self.assertNotEqual(one, two)
+
+    def test_add_ropes(self):
+        one = Rope("Hello,")
+        two = Rope([" world"])
+        rope = one + two
+        self.assertEqual(str(rope), str(one) + str(two))
+
+    def test_add_rope_and_string(self):
+        one = Rope("Hello,")
+        two = " world"
+        rope = one + two
+        self.assertEqual(str(rope), str(one) + str(two))
+    
+    def test_add_rope_and_list_string(self):
+        one = Rope("Hello,")
+        two = [" wo", "rld"]
+        rope = one + two
+        self.assertEqual(str(rope), "Hello, world")
+
+    def test_add_rope_and_int_throws_type_error(self):
+        one = Rope("Hello,")
+        two = 1
+        with self.assertRaises(TypeError):
+            rope = one + two
+
+    def test_len(self):
+        my_string = "Hello, world"
+        self.assertEqual(len(Rope(my_string)), len(my_string))
+    
+    def test_repr(self):
+        rope = Rope("Hello, world")
+        print(rope.__repr__())
+        self.assertEqual(rope.__repr__(), "Root: 'Hello, world'\n")
+
+    def test_get_item(self):
+        rope = Rope("Hello, world")
+        self.assertEqual(rope[0], 'H')
+        self.assertEqual(rope[-1], 'd')
+        self.assertEqual(rope[2], 'l')
+        self.assertEqual(rope[-2], 'l')
+
+    def test_get_item_with_larger_rope(self):
+        rope = Rope(["These ", "are ", "many ", "strings. ", "This", " rope", " should", " be", " taller!"])
+        self.assertEqual(rope[0], 'T')
+        self.assertEqual(rope[-1], '!')
+        self.assertEqual(rope[2], 'e')
+        self.assertEqual(rope[-2], 'r')
+        self.assertEqual(rope[6], 'a')
+        self.assertEqual(rope[10], 'm')
+    
+    def test_get_item_raises_type_error(self):
+        rope = Rope(["These ", "are ", "many ", "strings. ", "This", " rope", " should", " be", " taller!"])
+        with self.assertRaises(TypeError):
+            print(rope['1'])
+
+    def test_get_item_index_error(self):
+        rope = Rope("Hello, world")
+        with self.assertRaises(IndexError):
+            print(rope[25])
+
+    def test_split(self):
+        rope = Rope("Hello, world")
+        left, right = rope.split(5)
+        self.assertEqual(str(left), "Hello")
+        self.assertEqual(str(right), ", world")
+        self.assertEqual(len(left) + len(right), len(rope))
+
+    def test_split_out_of_bounds(self):
+        rope = Rope("Hello, world")
+        with self.assertRaises(IndexError):
+            left, right = rope.split(50)
+
+    def test_insert(self):
+        rope = Rope("Good Viet nam")
+        rope.insert(4, " morning")
+        self.assertEqual(str(rope), "Good morning Viet nam")
+
+    def test_performance_versus_str(self):
+        # Setup code
+        setup_code = """
+from main.rope import Rope
+
+import random
+import string
+
+length = 1000000  # Define the length of the large string
+base_str = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+rope = Rope(base_str)
+insert_str = " insert complex value :) "
+        """
+
+        # Code snippet for inserting into a Python string
+        base_str_test_code = """
+new_str = base_str
+for i in range(1000):  # Number of inserts
+    new_str = new_str[:50] + insert_str + new_str[50:]
+        """
+
+        # Code snippet for inserting into a Rope
+        rope_test_code = """
+for i in range(1000):  # Number of inserts
+    rope.insert(50, insert_str)
+        """
+
+        # Number of times the whole experiment is repeated
+        repeats = 5
+
+        # Time the base str insertions
+        base_str_time = timeit.timeit(stmt=base_str_test_code, setup=setup_code, number=repeats)
+
+        # Time the Rope insertions
+        rope_time = timeit.timeit(stmt=rope_test_code, setup=setup_code, number=repeats)
+
+        print(f"Average time for base str insertions: {base_str_time / repeats} seconds")
+        print(f"Average time for Rope insertions: {rope_time / repeats} seconds")
+
+if __name__ == '__main__':
+    unittest.main()
